be01f796a52913b332917abbda626f15afeed85a
Benedikt Langer <benedikt.langer@hotmail.com>
2022-05-25 09:47:10 +0200

    Created Project and implemented Tests and Model properties

---
+++ app.go
@@ -0,0 +1,38 @@
+package Go_Microservices
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *App) Initialize(user, password, dbname string) {
+
+	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
+}
+
+func (a *App) Run(addr string) {}
+func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w)
+	}
+}
